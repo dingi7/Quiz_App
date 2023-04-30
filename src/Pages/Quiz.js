fb561ae3120377f74fadea557f34ebfbb11b99bb
dingi7 <kamenkanev88@gmail.com>
2023-04-30 20:33:05 +0300

    Implemented logic to check the number of correct answers

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -9,13 +9,9 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react';
-import { useState, useContext, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useNavigate, useParams } from 'react-router-dom';
-
-//Contexts
-// import { QuestionsContext } from "../contexts/QuestionsContext"
-import { ResultContext } from '../contexts/ResultContext';
 
 //services
 import { getQuestionsByCategory } from '../services/requests';
@@ -33,20 +29,17 @@ export const QuizBox = () => {
     },
   ]);
 
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState();
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
   const [currentAnswer, setCurrentAnswer] = useState();
-
-  // const {category} = useContext(QuestionsContext)
-  const { correctAnswers, setCorrectAnswers, totalAnswers, setTotalAnswers } =
-    useContext(ResultContext);
+  const [answers, setAnswers] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
 
   useEffect(() => {
     // reset the result states when the component mounts
     setCorrectAnswers(0);
-    setTotalAnswers(0);
 
     getQuestionsByCategory(id).then(data => {
       setQuestions(data);
@@ -55,46 +48,37 @@ export const QuizBox = () => {
         alert('Empty quiz!');
       }
     });
-  }, [id, setCorrectAnswers, setTotalAnswers]);
+  }, [id, setCorrectAnswers]);
 
   const handleNextQuestions = () => {
     setCurrentQuestion(currentQuestion + 1);
+    setAnswers(state => [...state, parseInt(currentAnswer)]);
+    setValue('');
+    if(currentQuestion === questions.length - 1){
+      handleTestFinish()
+    }
   };
+
+  useEffect(() => {
+    console.log(correctAnswers);
+  }, [correctAnswers]);
 
   const handleTestFinish = () => {
+    questions.forEach((q, i) => {
+      if (parseInt(q.correctAnswer) === answers[i]) {
+        console.log(parseInt(q.correctAnswer) + " || " + answers[i]);
+        setCorrectAnswers(state => state + 1);
+      }
+    });
+
     alert('Завършихте теста');
 
-    // reset the result states when the user finishes the quiz
-    setCorrectAnswers(0);
-    setTotalAnswers(0);
-
-    // navigate("/")
-  };
-
-  const handleCurrentQuestion = () => {
-    if (questions[currentQuestion] === undefined) {
-      return;
-    }
-
-    // check if the answer is correct and update the result states
-    const selectedAnswerIndex = parseInt(currentAnswer) - 1;
-    const isAnswerCorrect =
-      questions[currentQuestion].answers[selectedAnswerIndex].correct;
-    if (isAnswerCorrect) {
-      setCorrectAnswers(correctAnswers + 1);
-    }
-    setTotalAnswers(totalAnswers + 1);
-
-    handleNextQuestions();
   };
 
   const onAnswerChange = e => {
     setCurrentAnswer(e.target.value);
+    console.log(e.target.value);
   };
-
-  if (questions[currentQuestion] === undefined) {
-    navigate('/');
-  }
 
   const currentQuestionData = questions[currentQuestion];
 
@@ -122,8 +106,9 @@ export const QuizBox = () => {
             <Stack marginLeft="5" justifyContent="center">
               {currentQuestionData.answers.map((answer, index) => (
                 <Radio
+                  // change key
                   key={answer.id}
-                  value={`${index + 1}`}
+                  value={`${index}`}
                   onChange={onAnswerChange}
                 >
                   {answer.text}
@@ -135,13 +120,13 @@ export const QuizBox = () => {
           <Stack marginLeft="5" direction="row">
             {currentQuestion === questions.length - 1 ? (
               <Button
-                onClick={handleTestFinish}
+                onClick={handleNextQuestions}
                 marginLeft="5"
                 marginRight="5"
                 marginBottom="2"
                 w="100%"
               >
-                Край
+                Завърши теста
               </Button>
             ) : (
               <Button
