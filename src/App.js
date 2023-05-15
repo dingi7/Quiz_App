1a4432d6f4849ec91f108de6031c7dc9e5de9614
dingi7 <kamenkanev88@gmail.com>
2023-05-15 22:11:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import {
-  Button,
   ChakraProvider,
   CloseButton,
   Flex,
